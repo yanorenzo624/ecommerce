b1e02d1007140c2518a04bc4b49880a55758365b
yanorenzo624 <yanorenzo624@gmail.com>
2026-01-06 00:09:43 -0800

    created a reusable loader component.

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center py-20">
+      <div className="h-10 w-10 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
